02ad95dada8fa38b83eb47a58e84138ba9ef4531
Bozhang Zhou <bozhangz@uci.edu>
2026-04-23 19:44:47 -0700

    update assignment1

--- a1.py
+++ a1.py
@@ -4,6 +4,34 @@
 
 # Replace the following placeholders with your information.
 
-# NAME
-# EMAIL
-# STUDENT ID
+# NAME Bozhang Zhou
+# EMAIL bozhangz@uci.edu
+# STUDENT ID 93213406
+
+from pathlib import Path
+import shlex
+
+def creat_file():
+
+
+
+def delete_file():
+
+
+
+def read_file():
+    
+
+
+
+
+def main():
+    while True:
+        text=input("C/D/R and file route, type Q to quit:")
+        if text=='Q':
+            break
+
+
+if __name__=="__main__":
+    user_command=''
+    main()
