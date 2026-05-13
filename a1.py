fc80458612f17f8aa66cb18337f14170f865935d
Bozhang Zhou <bozhangz@uci.edu>
2026-05-13 12:22:54 -0700

    Fixed style issues

--- a1.py
+++ a1.py
@@ -15,13 +15,13 @@ def create_file(text):
     if len(text) != 4 or text[2] != "-n":
         print ("ERROR")
         return
-    directory=Path(text[1])
+    directory = Path(text[1])
 
     if not directory.exists() or not directory.is_dir():
         print("ERROR")
         return
     else:
-        file_path=directory/(text[3]+'.dsu')
+        file_path = directory/(text[3]+'.dsu')
         if file_path.exists():
             print("ERROR")
             return
@@ -34,7 +34,7 @@ def delete_file(text):
         print("ERROR")
         return
     
-    file_path=Path(text[1])
+    file_path = Path(text[1])
 
     if not file_path.exists() or file_path.suffix != '.dsu' or not file_path.is_file():
         print("ERROR")
@@ -44,43 +44,44 @@ def delete_file(text):
     print(f'{file_path} DELETED')
 
 def read_file(text):
-    if len(text)!=2:
+    if len(text) != 2:
         print("ERROR")
         return
     
-    file_path=Path(text[1])
+    file_path = Path(text[1])
 
     if not file_path.exists() or not file_path.is_file() or file_path.suffix != ".dsu":
         print("ERROR")
         return
     else:
-        content=file_path.read_text()
+        content = file_path.read_text()
         
-        if content=='':
+        if len(content) == 0:
             print("EMPTY")
+            return
         else:
-            print(content)
+            print(content, end='')
 
 
 def main():
     while True:
-        text=input()
+        text = input()
         try:
-            order_parts=shlex.split(text)
+            order_parts = shlex.split(text)
         except ValueError:
-            print("Invalid input, please try again")
+            print("ERROR")
             continue
-        if len(order_parts)==0:
+        if len(order_parts) == 0:
             print("ERROR")
             continue
         
-        elif order_parts[0]=="Q":
+        elif order_parts[0] == "Q":
             break
-        elif order_parts[0]=="C":
+        elif order_parts[0] == "C":
             create_file(order_parts)
-        elif order_parts[0]=="D":
+        elif order_parts[0] == "D":
             delete_file(order_parts)
-        elif order_parts[0]=="R":
+        elif order_parts[0] == "R":
             read_file(order_parts)
         else:
             print("ERROR")
@@ -88,5 +89,5 @@ def main():
 
         
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
